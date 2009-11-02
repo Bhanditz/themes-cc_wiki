b963dc7da2d1e8a1f0e8bc6bfaf06fe13d52e918
Alex Roberts <alex@creativecommons.org>
2009-11-02 23:12:27 +0000

    Was Google Analytics really missing? Added.

--- CC.php
+++ CC.php
@@ -293,6 +293,16 @@ class CC extends QuickTemplate {
    
  </div>
 
+<!-- Google Analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-2010376-5");
+pageTracker._trackPageview();
+</script>
+
  <?php $this->html('bottomscripts'); /* JS call to runBodyOnloadHook */ ?>
  <?php $this->html('reporttime'); ?>
  <?php if ( $this->data['debug'] ) { ?>
