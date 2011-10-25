74526ade2a529c42c677cf58210b1609cf14bc6c
Nathan Kinkade <nkinkade@creativecommons.org>
2011-10-25 21:13:28 +0000

    Fixed a bad link in the footer for the license declaration.

--- CC.php
+++ CC.php
@@ -285,7 +285,7 @@ class CC extends QuickTemplate {
          <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
            <img src="http://i.creativecommons.org/l/by/3.0/88x31.png" alt="Creative Commons License" style="border:none;" height="31" width="88">
          </a>
-         Except where otherwise <a class="subfoot" href="/policies#license">noted</a>, content on this site is<br/>
+         Except where otherwise <a class="subfoot" href="http://creativecommons.org/policies#license">noted</a>, content on this site is<br/>
          licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/" class="subfoot">Creative Commons
          Attribution 3.0 License</a>
        </p>
